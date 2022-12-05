fda2e291e6776e6b2895cde742303ce559b8b9f1
Wiktor Latanowicz <wiktor@latanowicz.com>
2022-12-05 18:40:21 +0100

    Copy to clipboard

--- src/components/PasswordList.js
+++ src/components/PasswordList.js
@@ -1,5 +1,29 @@
+import "./PasswordList.css";
+
 import { allPasswords } from "../redux/selectors/passwords";
+import { copyTextToClipboard } from "../utils/clipboard";
 import { useSelector } from "react-redux";
+import { useState } from "react";
+
+const PasswordItem = ({ password }) => {
+  const [copied, setCopied] = useState(false);
+  const passwordClicked = () => {
+    setCopied(true);
+    copyTextToClipboard(password);
+    setTimeout(() => {
+      setCopied(false);
+    }, 1500);
+  };
+  return (
+    <p
+      className="password_item"
+      onClick={() => passwordClicked()}
+      title="Click to copy to clipboard"
+    >
+      {password} {copied && "copied"}
+    </p>
+  );
+};
 
 const PasswordList = (props) => {
   const passwords = useSelector(allPasswords);
@@ -10,7 +34,7 @@ const PasswordList = (props) => {
     <fieldset>
       <legend>Your random password{passwords.length > 1 ? "s" : ""}</legend>
       {passwords.map((password) => {
-        return <p>{password}</p>;
+        return <PasswordItem password={password} key={password} />;
       })}
     </fieldset>
   );
---
+++ src/utils/clipboard.js
@@ -0,0 +1,8 @@
+// copyright: https://blog.logrocket.com/implementing-copy-clipboard-react-clipboard-api/
+export async function copyTextToClipboard(text) {
+  if ("clipboard" in navigator) {
+    return await navigator.clipboard.writeText(text);
+  } else {
+    return document.execCommand("copy", true, text);
+  }
+}
